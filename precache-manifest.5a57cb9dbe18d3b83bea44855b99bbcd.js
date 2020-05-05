b1edb5224667353e9386d7a9903c5d0ce5bd69e0
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-05 19:06:01 +0530

    Updates

---
+++ precache-manifest.5a57cb9dbe18d3b83bea44855b99bbcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f104138dcbbaa572f8411d2aa96d0b2",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "307302fb9a747935aceb",
+    "url": "/react-clock/static/css/main.7a08fe92.chunk.css"
+  },
+  {
+    "revision": "e68c07d2190d28dafb6f",
+    "url": "/react-clock/static/js/2.39bfc00e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clock/static/js/2.39bfc00e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307302fb9a747935aceb",
+    "url": "/react-clock/static/js/main.5337bd33.chunk.js"
+  },
+  {
+    "revision": "88998f01e026fb0f55a7",
+    "url": "/react-clock/static/js/runtime-main.487398c0.js"
+  }
+]);
